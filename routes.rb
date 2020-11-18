2ae7ea6622ddfff485dd068342d12d63b35437f6
travis holt <trvslhlt@gmail.com>
2020-11-17 22:23:23 -0500

    add junk (#3)

--- routes.rb
+++ routes.rb
@@ -3,6 +3,14 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'sinatra/content_for'
 
+# vsdvsdf
+# sdfvsdfv
+#
+# sdf
+# v
+# sdfv
+
+
 get '/' do
   redirect '/summary'
 end
