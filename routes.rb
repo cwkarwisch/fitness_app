71ac170be5880f8318f682df016ab3b313114edd
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-17 21:56:46 -0500

    added initial routes
    - add root route
    - add summary route

--- routes.rb
+++ routes.rb
@@ -3,3 +3,11 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'sinatra/content_for'
 
+get '/' do
+  redirect '/summary'
+end
+
+get '/summary' do
+  "Summary to be added here."
+end
+
